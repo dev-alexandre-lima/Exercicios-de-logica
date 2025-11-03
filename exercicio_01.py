df47f114a153cc98489a00f16e9c8bd4acd9c926
Alexandre Lima <dev.alexandre96@gmail.com>
2025-11-03 00:17:14 -0300

    Create exercicio_01.py

---
+++ exercicio_01.py
@@ -0,0 +1,14 @@
+# Exercício 1: O Eco da Caverna (Definindo e Chamando)
+
+# Toda grande jornada começa com um passo... ou, neste caso, um som.
+# Objetivo: Criar sua primeira função.
+# Tarefa:
+
+# 1. Crie uma função chamada eco .
+def eco():
+    # 2. Esta função, quando chamada, deve simplesmente imprimir a string responde... olááá..." .
+    print("A caverna respende.....")
+    print("Oláááá.....")
+# 3. Chame a função "A cavernaeco() duas vezes para ter certeza de que funciona.
+eco()
+eco()
