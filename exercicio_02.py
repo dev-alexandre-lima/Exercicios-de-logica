6b333626b62a73173234304da288994823265d1a
Alexandre Lima <dev.alexandre96@gmail.com>
2025-11-03 00:11:18 -0300

    Create exercicio_02.py

---
+++ exercicio_02.py
@@ -0,0 +1,16 @@
+#Exercício 2: O Mapa do Tesouro (Parâmetros)
+
+# Você encontra um mapa, mas ele está em branco! Você precisa dizer ao mapa onde procurar.
+
+# Objetivo: Praticar o uso de parâmetros (dados que você envia para dentro da função).
+
+# Tarefa:
+
+#1. Crie uma função chamada marcar_no_mapa que aceita um parâmetro chamadolocal 
+def marcar_mapa(local):
+    #2. Dentro da função, imprima a mensagem: "Marcando o local: [local] no mapa."
+    print(F"Marcando o local: {local} no mapa")
+#3. Chame a função três vezes, passando locais diferentes: Goblin"), marcar_no_mapa("Sala do marcar_no_mapa("Poço das Armadilhas") e Dragão")
+marcar_mapa("Sala de Goblin")
+marcar_mapa("Poço de Armadilha")
+marcar_mapa("Cofre do Dragão")
